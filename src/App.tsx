e67e91251d8cb923a79f88fb8f0e3e727e21f2eb
tim.rohrer <tim@itstechnical.net>
2020-10-29 18:43:20 -0500

    feat: Add basic search element to App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,20 @@ import "./App.css";
 import Portfolio from "./features/portfolio/Portfolio";
 
 function App() {
+  const [searchTerm, setSearchTerm] = React.useState("");
+
+  const handleChange = (event: {
+    target: { value: React.SetStateAction<string> };
+  }) => {
+    console.log(setSearchTerm(event.target.value));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>My Investing</h1>
+        <label htmlFor="search">Search: </label>
+        <input id="search" type="text" onChange={handleChange} />
       </header>
       <Portfolio />
     </div>
