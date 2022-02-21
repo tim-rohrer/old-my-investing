3df4087fb33d161278d006cc5b51d2af3a6d7d8f
tim.rohrer <tim@tgr66.dev>
2022-02-21 17:56:21 -0600

    fix: Update module name

--- src/common/useStyles.ts
+++ src/common/useStyles.ts
@@ -1,4 +1,8 @@
-import { createStyles, fade, makeStyles } from "@material-ui/core/styles";
+import {
+  createStyles,
+  alpha,
+  makeStyles,
+} from "@material-ui/core/styles"
 
 export const useStyles = makeStyles((theme) =>
   createStyles({
@@ -17,9 +21,9 @@ export const useStyles = makeStyles((theme) =>
     search: {
       position: "relative",
       borderRadius: theme.shape.borderRadius,
-      backgroundColor: fade(theme.palette.common.white, 0.15),
+      backgroundColor: alpha(theme.palette.common.white, 0.15),
       "&:hover": {
-        backgroundColor: fade(theme.palette.common.white, 0.25),
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
       },
       marginLeft: 0,
       width: "100%",
@@ -86,5 +90,5 @@ export const useStyles = makeStyles((theme) =>
       height: 150,
       width: "100%",
     },
-  })
-);
+  }),
+)
