00709a2f84ec640ee1801a022a2a7b40bea84ad8
tim.rohrer <tim@tgr66.dev>
2022-02-21 21:15:52 -0600

    test: Remove beforeEach until tests fixed; styling

--- src/features/companyAnalysis/useCompanyProfile.test.ts
+++ src/features/companyAnalysis/useCompanyProfile.test.ts
@@ -1,21 +1,21 @@
-import configureMockStore from "redux-mock-store";
-import thunk from "redux-thunk";
-import * as reactRedux from "react-redux";
-import { renderHook, act } from "@testing-library/react-hooks";
-import { render } from "../../test-utils";
-import useCompanyProfile from "./useCompanyProfile";
+import configureMockStore from "redux-mock-store"
+import thunk from "redux-thunk"
+import * as reactRedux from "react-redux"
+import { renderHook, act } from "@testing-library/react-hooks"
+import { render } from "../../test-utils"
+import useCompanyProfile from "./useCompanyProfile"
 
-const middlewares = [thunk];
-const mockStore = configureMockStore(middlewares);
+const middlewares = [thunk]
+const mockStore = configureMockStore(middlewares)
 
 describe("Company Profile Hook", () => {
-  const useSelectorMock = jest.spyOn(reactRedux, "useSelector");
-  const useDispatchMock = jest.spyOn(reactRedux, "useDispatch");
+  const useSelectorMock = jest.spyOn(reactRedux, "useSelector")
+  const useDispatchMock = jest.spyOn(reactRedux, "useDispatch")
 
-  beforeEach(() => {
-    useSelectorMock.mockClear();
-    useDispatchMock.mockClear();
-  });
+  // beforeEach(() => {
+  //   useSelectorMock.mockClear();
+  //   useDispatchMock.mockClear();
+  // });
 
   // it("should return profile from store if previously fetched", async () => {
   //   useSelectorMock.mockReturnValue(["AAPL", "SPY"]);
@@ -47,6 +47,6 @@ describe("Company Profile Hook", () => {
   // });
 
   it("should fetch and return profile if not previously loaded", () => {
-    expect(1).toBe(1);
-  });
-});
+    expect(1).toBe(1)
+  })
+})
