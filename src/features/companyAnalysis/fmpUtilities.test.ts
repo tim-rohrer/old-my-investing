6c232f09265ecdb4d2aaf8e89b72916e279a2c82
tim.rohrer <tim@tgr66.dev>
2022-02-21 20:11:58 -0600

    test: Fix types for tests and styling

--- src/features/companyAnalysis/fmpUtilities.test.ts
+++ src/features/companyAnalysis/fmpUtilities.test.ts
@@ -1,12 +1,12 @@
-import nock from "nock";
+import nock from "nock"
 import {
   convertFMPTradableSymbolListToBySymbolObject,
   fetchFMPData,
-} from "./fmpUtilities";
-import axios from "axios";
-import { FMPRequestObject } from "./companyProfilesSlice";
-import { SecurityList } from "../securities/securitiesSlice";
-axios.defaults.adapter = require("axios/lib/adapters/http");
+} from "./fmpUtilities"
+import axios from "axios"
+import { FMPRequestObject } from "./companyProfilesSlice"
+import { SecurityList } from "../securities/securitiesSlice"
+axios.defaults.adapter = require("axios/lib/adapters/http")
 
 describe("FMP Utilities", () => {
   describe("Company Valuation/Profile", () => {
@@ -29,77 +29,84 @@ describe("FMP Utilities", () => {
           "Apple Inc is designs, manufactures and markets mobile communication and media devices and personal computers, and sells a variety of related software, services, accessories, networking solutions and third-party digital content and applications.",
         ceo: "Timothy D. Cook",
         sector: "Technology",
-        image: "https://financialmodelingprep.com/images-New-jpg/AAPL.jpg",
+        image:
+          "https://financialmodelingprep.com/images-New-jpg/AAPL.jpg",
       },
-    };
+    }
 
     describe("fetchFMPData", () => {
       const scope = nock("https://financialmodelingprep.com")
         .defaultReplyHeaders({ "access-control-allow-origin": "*" })
-        .persist();
+        .persist()
 
-      const apiKey = process.env.REACT_APP_FMP_API_KEY;
-      const path = `/api/v3/company/profile/AAPL?apikey=${apiKey}`;
+      const apiKey = process.env.REACT_APP_FMP_API_KEY
+      let path = `/api/v3/company/profile/AAPL?apikey=${apiKey}`
 
       it("handles a fmpRequestObject", async () => {
-        scope.get(path).reply(200, mockedFMPCompanyProfileApple);
+        scope.get(path).reply(200, mockedFMPCompanyProfileApple)
 
         const fmpRequestObject: FMPRequestObject = {
           requestType: "companyProfile",
           securitySymbol: "AAPL",
-        };
-        const actual = await fetchFMPData(fmpRequestObject);
+        }
+        const actual = await fetchFMPData(fmpRequestObject)
         expect(actual.profile.companyName).toEqual(
-          mockedFMPCompanyProfileApple.profile.companyName
-        );
-      });
+          mockedFMPCompanyProfileApple.profile.companyName,
+        )
+      })
 
       it("handles return of securitySymbol", async () => {
-        scope.get(path).reply(200, mockedFMPCompanyProfileApple);
+        scope.get(path).reply(200, mockedFMPCompanyProfileApple)
 
         const fmpRequestObject: FMPRequestObject = {
           requestType: "companyProfile",
           securitySymbol: "AAPL",
-        };
+        }
 
-        const actual = await fetchFMPData(fmpRequestObject);
+        const actual = await fetchFMPData(fmpRequestObject)
         // console.log(actual)
 
-        expect(actual).toEqual(mockedFMPCompanyProfileApple);
-      });
+        expect(actual).toEqual(mockedFMPCompanyProfileApple)
+      })
 
       it("handles a request of an unfound symbol", async () => {
         const fmpRequestObject: FMPRequestObject = {
           requestType: "companyProfile",
           securitySymbol: "UNFOUND",
-        };
-        const path = `/api/v3/company/profile/${fmpRequestObject.securitySymbol}?apikey=${apiKey}`;
-        scope.get(path).reply(200, {});
-
-        expect.assertions(1);
-        try {
-          await fetchFMPData(fmpRequestObject);
-        } catch (e) {
-          // console.log(e.message);
-          expect(e.message).toEqual("Symbol not found");
         }
-      });
+        path = `/api/v3/company/profile/${fmpRequestObject.securitySymbol}?apikey=${apiKey}`
+        scope.get(path).reply(200, {})
+
+        expect.assertions(2)
+        try {
+          await fetchFMPData(fmpRequestObject)
+        } catch (e) {
+          expect(e).toBeInstanceOf(Error)
+          let errorMessage = ""
+          if (e instanceof Error) errorMessage = e.message
+          expect(errorMessage).toEqual("Symbol not found")
+        }
+      })
 
       it("handles return of an error", async () => {
-        scope.get(path).reply(500);
+        scope.get(path).reply(500)
 
+        expect.assertions(1)
         try {
           await fetchFMPData({
             requestType: "companyProfile",
             securitySymbol: "AAPL",
-          });
+          })
         } catch (e) {
-          // console.log(e);
-          expect(e.message).toEqual("Request failed with status code 500");
+          let errorMessage = ""
+          if (e instanceof Error) errorMessage = e.message
+          expect(errorMessage).toEqual(
+            "Request failed with status code 500",
+          )
         }
-      });
-    });
-  });
+      })
+    })
+  })
 
   describe("Convert securities form", () => {
     it("should convert FMP Tradable Symbols List to My Investing Securities List", () => {
@@ -116,7 +123,7 @@ describe("FMP Utilities", () => {
           price: 56.41,
           exchange: "Nasdaq Global Select",
         },
-      ];
+      ]
       const myInvestingSecuritiesList: SecurityList = {
         SPY: {
           symbol: "SPY",
@@ -128,11 +135,13 @@ describe("FMP Utilities", () => {
           name: "Comcast Corp",
           exchange: "Nasdaq Global Select",
         },
-      };
+      }
 
       expect(
-        convertFMPTradableSymbolListToBySymbolObject(tradableListFixture)
-      ).toStrictEqual(myInvestingSecuritiesList);
-    });
-  });
-});
+        convertFMPTradableSymbolListToBySymbolObject(
+          tradableListFixture,
+        ),
+      ).toStrictEqual(myInvestingSecuritiesList)
+    })
+  })
+})
