c3b2e428d890cd4acb1f4f0a524e5e0a31eae062
tim.rohrer <tim@tgr66.dev>
2022-02-21 21:50:01 -0600

    test: Styling

--- src/features/system/systemSlice.test.ts
+++ src/features/system/systemSlice.test.ts
@@ -1,7 +1,7 @@
-import { cleanup } from "../../common/test-utils.js";
-import system, { appIsLoaded, appIsThinking } from "./systemSlice";
+import { cleanup } from "../../common/test-utils.js"
+import system, { appIsLoaded, appIsThinking } from "./systemSlice"
 
-afterEach(cleanup);
+afterEach(cleanup)
 
 describe("System Slice", () => {
   it("should return the initial state", () => {
@@ -9,81 +9,81 @@ describe("System Slice", () => {
       userId: "Demo", // Needs to be implemented in actions.
       appThinking: false,
       appLoaded: false,
-    };
+    }
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    expect(system(undefined, { type: "@@INIT" } as any)).toStrictEqual(
-      testInitialState
-    );
-  });
+    expect(
+      system(undefined, { type: "@@INIT" } as any),
+    ).toStrictEqual(testInitialState)
+  })
 
   it("should set appLoaded to true", () => {
-    const testArg = true;
-    const testPayload = testArg;
-    let testState: undefined;
+    const testArg = true
+    const testPayload = testArg
+    let testState: undefined
     const expectedMatch: Record<string, unknown> = {
       userId: "Demo",
       appThinking: false,
       appLoaded: testArg,
-    };
-    const actionCreator = appIsLoaded;
+    }
+    const actionCreator = appIsLoaded
 
-    const testResult = system(testState, actionCreator(testPayload));
+    const testResult = system(testState, actionCreator(testPayload))
 
-    expect(testResult).toStrictEqual(expectedMatch);
-  });
+    expect(testResult).toStrictEqual(expectedMatch)
+  })
   it("should set appLoaded to false", () => {
-    const testArg = false;
-    const testPayload = testArg;
-    let testState: undefined;
+    const testArg = false
+    const testPayload = testArg
+    let testState: undefined
     const expectedMatch: Record<string, unknown> = {
       userId: "Demo",
       appThinking: false,
       appLoaded: testArg,
-    };
-    const actionCreator = appIsLoaded;
+    }
+    const actionCreator = appIsLoaded
 
-    const testResult = system(testState, actionCreator(testPayload));
+    const testResult = system(testState, actionCreator(testPayload))
 
-    expect(testResult).toStrictEqual(expectedMatch);
-  });
+    expect(testResult).toStrictEqual(expectedMatch)
+  })
 
   it("should set appThinking to true", () => {
-    const testArg = true;
-    const testPayload = testArg;
+    const testArg = true
+    const testPayload = testArg
     const testState = {
       userId: "Demo",
       appThinking: false,
       appLoaded: true,
-    };
+    }
     const expectedMatch: Record<string, unknown> = {
       userId: "Demo",
       appThinking: testArg,
       appLoaded: true,
-    };
-    const actionCreator = appIsThinking;
+    }
+    const actionCreator = appIsThinking
 
-    const testResult = system(testState, actionCreator(testPayload));
+    const testResult = system(testState, actionCreator(testPayload))
 
-    expect(testResult).toStrictEqual(expectedMatch);
-  });
+    expect(testResult).toStrictEqual(expectedMatch)
+  })
 
   it("should set appThinking to false", () => {
-    const testArg = false;
-    const testPayload = testArg;
+    const testArg = false
+    const testPayload = testArg
     const testState = {
       userId: "Demo",
       appThinking: true,
       appLoaded: true,
-    };
+    }
     const expectedMatch: Record<string, unknown> = {
       userId: "Demo",
       appThinking: testArg,
       appLoaded: true,
-    };
-    const actionCreator = appIsThinking;
+    }
+    const actionCreator = appIsThinking
 
-    const testResult = system(testState, actionCreator(testPayload));
+    const testResult = system(testState, actionCreator(testPayload))
 
-    expect(testResult).toStrictEqual(expectedMatch);
-  });
-});
+    expect(testResult).toStrictEqual(expectedMatch)
+  })
+})
