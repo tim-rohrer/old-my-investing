4a6264bcf5c6d3ea6ec221e776be8cfe18dc934d
tim.rohrer <tim@tgr66.dev>
2022-06-26 16:29:54 -0400

    test: Comment out for now.

--- src/features/companyAnalysis/useCompanyProfile.test.ts
+++ src/features/companyAnalysis/useCompanyProfile.test.ts
@@ -1,52 +1,52 @@
-import configureMockStore from "redux-mock-store"
-import thunk from "redux-thunk"
-import * as reactRedux from "react-redux"
-import { renderHook, act } from "@testing-library/react-hooks"
-import { render } from "../../test-utils"
-import useCompanyProfile from "./useCompanyProfile"
+// import configureMockStore from "redux-mock-store"
+// import thunk from "redux-thunk"
+// import * as reactRedux from "react-redux"
+// // import { renderHook, act } from "@testing-library/react-hooks"
+// import { render } from "../../test-utils"
+// import useCompanyProfile from "./useCompanyProfile"
 
-const middlewares = [thunk]
-const mockStore = configureMockStore(middlewares)
+// const middlewares = [thunk]
+// const mockStore = configureMockStore(middlewares)
 
-describe("Company Profile Hook", () => {
-  const useSelectorMock = jest.spyOn(reactRedux, "useSelector")
-  const useDispatchMock = jest.spyOn(reactRedux, "useDispatch")
+// describe("Company Profile Hook", () => {
+//   const useSelectorMock = jest.spyOn(reactRedux, "useSelector")
+//   const useDispatchMock = jest.spyOn(reactRedux, "useDispatch")
 
-  // beforeEach(() => {
-  //   useSelectorMock.mockClear();
-  //   useDispatchMock.mockClear();
-  // });
+//   // beforeEach(() => {
+//   //   useSelectorMock.mockClear();
+//   //   useDispatchMock.mockClear();
+//   // });
 
-  // it("should return profile from store if previously fetched", async () => {
-  //   useSelectorMock.mockReturnValue(["AAPL", "SPY"]);
-  //   useDispatchMock.mockReturnValue({});
+//   // it("should return profile from store if previously fetched", async () => {
+//   //   useSelectorMock.mockReturnValue(["AAPL", "SPY"]);
+//   //   useDispatchMock.mockReturnValue({});
 
-  //   const store = mockStore({
-  //     companyProfiles: {
-  //       profiles: {
-  //         AAPL: {
-  //           symbol: "AAPL",
-  //           name: "Apple",
-  //         },
-  //       },
-  //     },
-  //   });
+//   //   const store = mockStore({
+//   //     companyProfiles: {
+//   //       profiles: {
+//   //         AAPL: {
+//   //           symbol: "AAPL",
+//   //           name: "Apple",
+//   //         },
+//   //       },
+//   //     },
+//   //   });
 
-  //   const { result, waitForNextUpdate } = renderHook(
-  //     () => useCompanyProfile("AAPL"),
-  //     { wrapper: render.Wrapper }
-  //   );
+//   //   const { result, waitForNextUpdate } = renderHook(
+//   //     () => useCompanyProfile("AAPL"),
+//   //     { wrapper: render.Wrapper }
+//   //   );
 
-  //   result.current.fetchFMPCompanyProfileBySymbol();
+//   //   result.current.fetchFMPCompanyProfileBySymbol();
 
-  //   await waitForNextUpdate();
+//   //   await waitForNextUpdate();
 
-  //   expect(result.current).toMatchObject({
-  //     symbol: "AAPL",
-  //   });
-  // });
+//   //   expect(result.current).toMatchObject({
+//   //     symbol: "AAPL",
+//   //   });
+//   // });
 
-  it("should fetch and return profile if not previously loaded", () => {
-    expect(1).toBe(1)
-  })
-})
+//   it("should fetch and return profile if not previously loaded", () => {
+//     expect(1).toBe(1)
+//   })
+// })
